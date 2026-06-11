4bd3d80ba7b1b72bd9cd4acd8192e42d81f88224
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-06-11 15:36:04 +0530

    updated file

--- printNumbers.cpp
+++ printNumbers.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 void print(int mat[][3],int n,int m){
+    int count=0;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             if(mat[i][j]==7){
                 cout<<i<<","<<j<<endl;
+                count++;
             }
         }
     }
+    cout<<"Total occurrences of 7: "<<count<<endl;
 }
 
 int main(){
