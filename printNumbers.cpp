999bbc93fa722dd4dee19872e316d289d494fd71
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-06-11 15:32:53 +0530

    First Commit

---
+++ printNumbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void print(int mat[][3],int n,int m){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(mat[i][j]==7){
+                cout<<i<<","<<j<<endl;
+            }
+        }
+    }
+}
+
+int main(){
+    int mat[2][3]={ {4,7,8},
+                    {8,8,7} };
+    print(mat,2,3);
+    return 0;
+
+
+}
