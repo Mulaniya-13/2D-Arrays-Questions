de5fb36a4d30d6fd8ece39a37a969ccee524b41c
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-06-17 21:47:06 +0530

    updating existing file

--- searchA2DMatrix_LC-74.cpp
+++ searchA2DMatrix_LC-74.cpp
@@ -1,12 +1,33 @@
 #include<iostream>
 using namespace std;
 
+//linear search T.C: O(m*n) S.C: O(1)
+// bool searchMatrix(int mat[][4],int m,int n,int target){
+//     for(int i=0;i<m;i++){
+//         for(int j=0;j<n;j++){
+//             if(mat[i][j]==target){
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+//binary search T.C: O(log(m*n)) S.C: O(1)
 bool searchMatrix(int mat[][4],int m,int n,int target){
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(mat[i][j]==target){
-                return true;
-            }
+    int low=0;
+    int high=m*n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        int midElement=mat[mid/n][mid%n];
+        if(midElement==target){
+            return true;
+        }
+        else if(midElement<target){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
         }
     }
     return false;
